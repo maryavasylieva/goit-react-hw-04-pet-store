d006c05cdb53eec364ef6484d445c0ec3c507f19
maryavasylieva <miheta.laurelin@gmail.com>
2019-08-17 00:58:49 +0300

    rw

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import { HashRouter, Route } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
-  <HashRouter basename="/">
-    <Route component={App} />
-  </HashRouter>,
-  document.getElementById('root'),
-);
-
 // ReactDOM.render(
-//   <BrowserRouter>
+//   <HashRouter basename="/">
 //     <Route component={App} />
-//   </BrowserRouter>,
+//   </HashRouter>,
 //   document.getElementById('root'),
 // );
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Route component={App} />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
